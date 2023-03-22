9c11bb47680170a36c115bfe32565f5dcda6cedc
Romulo Alvernaz <127262652+RomuloAlvernaz@users.noreply.github.com>
2023-03-22 17:52:27 -0300

    :alien: Update Grid
    Criação do Grid e dos movimentos dos Invasores

--- script.js
+++ script.js
@@ -87,7 +87,7 @@ class Projectile {
 }
 
 class Invader {
-    constructor() {
+    constructor({position}) {
         
         this.velocity = {
             x: 0,
@@ -103,8 +103,8 @@ class Invader {
             this.width = image.width * scale
             this.height = image.height * scale
             this.position = {
-                x: canvas.width / 2 - this.width / 2,
-                y: canvas.height / 2
+                x: position.x,
+                y: position.y
             }
         }
     }
@@ -122,11 +122,11 @@ class Invader {
         )
     }
 
-    update () {
+    update ({velocity}) {
         if (this.image) {
             this.draw()
-            this.position.x += this.velocity.x
-            this.position.y += this.velocity.y
+            this.position.x += velocity.x
+            this.position.y += velocity.y
         }  
     }
 }
@@ -139,14 +139,43 @@ class Grid {
         }
 
         this.velocity = {
-            x: 0,
+            x: 3,
             y: 0
         }
 
-        this.invaders = [new Invader()]
+        this.invaders = []
+
+        const columns = Math.floor(Math.random() * 10 + 5)
+        const rows = Math.floor(Math.random() * 5 + 2)
+
+        this.width = columns * 30
+
+        for (let x = 0; x < columns; x++) {
+            for (let y = 0; y < rows; y++) {
+                this.invaders.push(
+                    new Invader({
+                        position: {
+                            x: x * 30,
+                            y: y * 30
+                        }
+                    })
+                )
+            }
+        }
+        console.log(this.invaders)
     }
 
-    update() {}
+    update() {
+        this.position.x += this.velocity.x
+        this.position.y += this.velocity.y
+
+        this.velocity.y = 0
+
+        if(this.position.x + this.width >= canvas.width || this.position.x <= 0) {
+            this.velocity.x = -this.velocity.x
+            this.velocity.y = 30
+        }
+    }
 }
 
 const player = new Player()
@@ -184,7 +213,7 @@ function animate() {
     grids.forEach(grid => {
         grid.update()
         grid.invaders.forEach(invader => {
-            invader.update()
+            invader.update({velocity: grid.velocity})
         })
     })
 
